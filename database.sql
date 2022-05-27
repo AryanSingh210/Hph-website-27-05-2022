c213e76a5a72e7cd9f18771d5c40ac765c1d97fe
AryanSingh210 <93995400+AryanSingh210@users.noreply.github.com>
2022-05-27 20:13:22 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,11 @@
+create database hph;
+
+use hph;
+
+create table hph_table(
+    Name varchar(200)
+    Email varchar (200)
+    Phone Bigint
+    Comment varchar(1000)
+    Remarks varchar(30)
+);
